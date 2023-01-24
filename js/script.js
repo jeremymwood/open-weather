20b76dbe285baecfedaf54403a9f375225c18aaf
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-01-24 11:30:51 -0600

    1130

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@
         //use reverse geocode for city and state
         //general weather icons
         //fix wind direction
+        //readme
 
         //wind and temp widget
         //mobile responsive
@@ -180,7 +181,6 @@
         let e = $('<div id="testForecast" class="currentData border border-2 border-white text-white rounded-3 bg-dark fs-6 mt-2 p-2"></div>');
         $('body').append(e);
 
-
         let f = $('<div id="coordinates" class="coordinates border border-2 border-white text-white rounded-3 bg-dark fs-6 mt-2 p-2"></div>');
         $('body').append(f);
 
@@ -232,130 +232,4 @@
     //     });
     // }
 
-    // introduce .ajax()
-    // go over request/response architecture
-    // WHITEBOARD!
-    // mention different types of requests
-    // mention different types of data
-    // show https://java.codeup.com as an example request and show the response as html
-
-    // make a simple AJAX request to http://localhost
-    // and show the echo server
-    // $.ajax("http://localhost/?sort=desc&state=TX");
-    //
-    // // do a POST
-    // $.ajax("http://localhost/", {
-    //     type: "POST",
-    //     dataType: "json",
-    //     contentType: "application/json",
-    //     data: JSON.stringify({
-    //         name: "Bob Smith",
-    //         age: 42
-    //     })
-    // });
-    //
-    // // do a PUT
-    // $.ajax("http://localhost/v1/people/555", {
-    //     type: "PUT",
-    //     dataType: "json",
-    //     contentType: "application/json",
-    //     data: JSON.stringify({
-    //         name: "Bob Smith",
-    //         age: 52
-    //     })
-    // });
-    //
-    //     // do a DELETE
-    //
-    // $.ajax("http://localhost", {
-    //         type: "DELETE",
-    //         dataType: "json"
-    // });
-
-    // do some method chaining
-    // use .done() to see the data from the GET request
-
-    //     $.ajax("http://localhost").done(function(data) {
-    //         // iterate over the data array
-    //         // and print out each thing
-    //         // console.log(data);
-    //         // for (const row of data) {
-    //         //     console.log(row);
-    //         // }
-    //         $("#my-div").text(data[0].currentPet.age);
-    //
-    // }).fail(function(jqXhr, status, error) {
-    //         console.log("crap! "  + jqXhr.status)
-    //     });
-
-
-    // use .fail() and make a 404 happen
-    // be sure to print the status
-    // mutually exclusive with done()
-
-    // show all the pretty http response codes
-
-    // function getQuote() {
-    //     $("#anime").text("");
-    //     $("#character").text("");
-    //     $("#quote").text("");
-    //     $("#loading-pic").removeClass("d-none");
-    //     $.ajax("https://animechan.vercel.app/api/random")
-    //         .done(function(data) {
-    //             console.log(data);
-    //             $("#anime").text(data.anime);
-    //             $("#character").text(data.character);
-    //             $("#quote").text(data.quote);
-    //
-    //         }).fail(function(error) {
-    //         console.log("RATS! an error");
-    //     }).always(function() {
-    //         $("#loading-pic").addClass("d-none");
-    //     })
-    // }
-    //
-    // $("#get-quote").click(getQuote);
-
-    // $.ajax("https://pokeapi.co/api/v2/pokemon/charmander")
-    //     .done(function(data) {
-    //         // console.log(data);
-    //         // $("#p1").text(data.name);
-    //         // $("#p2").text(data.height);
-    //         // $("#p3").text(data.weight);
-    //     })
-    //     .fail(function(statusCode) {
-    //         console.log(statusCode.status);
-    //     });
-    // add .always() and explain
-
-    // point out get and post shorthand
-
-    // use a real api!
-
-
-    // introduce JSON MAYBE DO THIS LAST BEFORE ANIME QUOTE DEMO
-    // show JS object and talk about how it may not be usable outside of JS
-    // show JSON equivalent
-
-
-
-    // no cors problems THANK GOD!
-
-
-    // HOWEVER: FOR 5 DAY FORECAST this api is not what we want
-    // we must use the one call endpoint!
-    // https://openweathermap.org/api/one-call-api#parameter
-    // notice we must now use lat/lon instead of the city name + state
-
-    // where is our data?!?
-
-    // look in the .daily property
-    // .daily is an array with 8 elements
-    // index 0 is today
-    // index 1 is tomorrow, etc.
-
-    // what is dt ?  how to convert it to something more recognizable?
-    // https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
-
-    // what about the weather icon??? check out .daily[x].weather[0].icon
 })();
