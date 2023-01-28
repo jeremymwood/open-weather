39e30a614bfaf0766aee0812cf3ec37d3c6b26cf
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-01-27 22:22:14 -0600

    2222

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
         let c = $('<div class="container-fluid text-white fs-6 m-auto p-0"><div id="liveLocation" class="row"></div></div>');
         $('body').prepend(c);
 
-        let d = $('<header><h1 class="text-white text-center">TriCast</h1></header>');
+        let d = $('<header><h1 class="text-white text-center">Vesicast Weather</h1></header>');
         $('body').prepend(d);
 
-        let e = $('<mapbox-address-autofill access-token="MAPBOX_API_KEY" class="searchContainerParent"><div class="searchContainer m-0"><i class="fa-solid fa-magnifying-glass text-white" aria-hidden="true"></i><form><input id="search" class="search border border-1 border-white text-white rounded-3 bg-dark fs-6 px-5 py-1 mb-2" type="text" name="address" autocomplete="shipping street-address" placeholder="search location...">    <button id="myBtn" class="d-none" type="submit">button</button>\n</form></div></mapbox-address-autofill>');
+        let e = $('<mapbox-address-autofill access-token="MAPBOX_API_KEY" class="searchContainerParent"><div class="searchContainer m-0"><i class="fa-solid fa-magnifying-glass text-secondary" aria-hidden="true"></i><form><input id="search" class="search border border-1 border-white text-white rounded-3 bg-dark fs-6 ps-5 pe-1 py-1 mb-2" type="text" name="address" autocomplete="shipping street-address" placeholder="search location...">    <button id="myBtn" class="d-none" type="submit">button</button>\n</form></div></mapbox-address-autofill>');
         $('header').append(e);
 
         // TODO:
@@ -86,7 +86,8 @@
 
                 //formatted time
                 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-                const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                // const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                const months = ["Jan", "Feb", "Ma", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
                 let i = 0;
                 let unix_timestamp = data.list[i].dt;
@@ -177,10 +178,30 @@
                             ${conditionMachina(conditionMain)}
                         </div>
                         ${namedMonth} ${dayOfMonth}, ${year}
-                        <p class="fcConditionIcon m-0">(${data.list[i].weather[0].description})</p>
-                        <p class="fcTemps m-0">High: ${Math.round(data.list[i].main.temp_max)}°/ Low: ${Math.round(data.list[i].main.temp_min)}°</p>
+                        <p class="fcConditionIcon m-0">(${data.list[i].weather[0].description})
+                        <div class="hiLo d-flex">
+                            <p class="fcTemps m-0">
+                                <i class="forecast-icons fa-solid fa-temperature-arrow-up"></i>
+                            </p>
+                            <p class="fcTemps m-0 ps-2">
+                                ${Math.round(data.list[i].main.temp_max)}°
+                            </p>
+                            <p class="fcTemps m-0">
+                                <i class="forecast-icons fa-solid fa-temperature-arrow-down ps-3"></i>
+                            </p>
+                            <p class="fcTemps m-0 ps-2">
+                                ${Math.round(data.list[i].main.temp_min)}°
+                            </p>
+                        </div>
                         <p class="fcHumidity m-0">Humidity: ${data.list[i].main.humidity}%</p>
-                        <p class="fcWind m-0">Wind: ${Math.round(data.list[i].wind.speed)} mph, ${degToCompass(rawWind)}</p>
+                        <div class="windContainer d-flex">
+                            <p class="fcWind m-0 pe-2">
+                                <i class="forecast-icons fa-solid fa-wind"></i>
+                            </p>
+                            <p class="fcWind m-0">
+                                ${Math.round(data.list[i].wind.speed)} mph, ${degToCompass(rawWind)}
+                            </p>
+                        </div>
                         <p class="fcGust m-0"> Gust: ${Math.round(data.list[i].wind.gust)} mph</p>`;
 
                         document.getElementById(dayId).innerHTML = fcHtml;
@@ -200,7 +221,7 @@
             '        <div class="container-fluid d-flex p-0">\n' +
             '                <div class="footer40 m-0 p-2">\n' +
             '                    <h6 class="mt-2 mb-0">About</h6>\n' +
-            '                    <p class="text-justify pt-3">TriCast provides reliable weather data in three hour increments because hourly forecasts are excessive and that API option is cost prohibitive to our valued customers!</p>\n' +
+            '                    <p class="text-justify pt-3">Vesicast Weather provides reliable weather data in three hour increments because hourly forecasts are excessive and that API option is cost prohibitive to our valued customers!</p>\n' +
             '                </div>\n' +
             '\n' +
             '                <div class="footer20 m-0 p-2">\n' +
@@ -227,7 +248,7 @@
             '                        <li class="m-0"><a class="linkedin ms-0 me-2" href="#"><i class="fa fa-linkedin m-0"></i></a></li>\n' +
             '                    </ul>\n' +
             '                    <p class="copyright-text mt-2">Copyright &copy; 2023 All Rights Reserved by\n' +
-            '                        <a href="#">TriCast</a>.\n' +
+            '                        <a href="#">Vesicast Weather</a>.\n' +
             '                    </p>\n' +
             '                </div>\n' +
             '        </div>\n' +
